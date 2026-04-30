5480a6b58612be42a609e122f7dd624332a4e4e1
Gustavo <gustavooliveiracosta349@gmail.com>
2026-04-30 14:19:03 -0300

    1 - Criando o projeto ProjetoNineGames

---
+++ ProjetoNineGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoNineGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
