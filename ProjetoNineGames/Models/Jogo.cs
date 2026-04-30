a8ae0415210d67ed2f38efb99c719f1f23d1f460
Gustavo <gustavooliveiracosta349@gmail.com>
2026-04-30 14:37:37 -0300

    3 - Criando as models Jogo e Usuario

---
+++ ProjetoNineGames/Models/Jogo.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoNineGames.Models
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string Categoria { get; set; }
+    }
+}
---
+++ ProjetoNineGames/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjetoNineGames.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        //HASH da senha
+        public string SenhaHash { get; set; }
+
+        // Campos para a Autenticação em Duas Etapas (2FA)
+        // O Secret é a chave que o App Flutter usará para gerar os códigos
+        public string TwoFactorSecret { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+
+        public DateTime CriadoEm { get; set; }
+    }
+}
