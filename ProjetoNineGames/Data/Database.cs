790ff52dea501fb18a393f41095e480ce2b7fe94
Gustavo <gustavooliveiracosta349@gmail.com>
2026-04-30 14:39:16 -0300

    4 - Criando a pasta Data e criando a classe database para fazer a conexão com o banco de dados

---
+++ ProjetoNineGames/Data/Database.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ProjetoNineGames.Data
+{
+    public class Database
+    {
+        private readonly string connectionString = "server=localhost;port=3306;database=bdloja_games;user=root;password=12345678;";
+
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
